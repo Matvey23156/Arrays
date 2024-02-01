e0b8d893e308c6159509cfc4538da2137d56c06a
Matvey Kolomov <matvey.kolomov134@gmail.com>
2024-02-01 17:04:30 +1000

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Создать массив и заполнить его целыми числами от 1 до N. Вывести элементы массива на экран.
+// Пример: N = 4 => 1 2 3 4; T = 6 => 1 2 3 4 5 6 
+
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < n)
+{
+    arr[i] = i + 1;
+    // System.Console.Write(arr[i]);
+    // System.Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+    
+}
